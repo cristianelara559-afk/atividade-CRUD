2ee34a33b5b44d99370acfc4f5d1fb6a53a2860c
LARAH SOUZA DA SILVA <larah_silva@estudante.sesisenai.org.br>
2026-07-20 17:06:33 -0300

    função listar aluno

--- ex1.js
+++ ex1.js
@@ -48,3 +48,17 @@ function cadastrarAluno() {
   alert(`Aluno "${nome}" cadastrado com sucesso!`);
 }
 
+function listarAlunos() {
+  if (alunos.length === 0) {
+    alert("Nenhum aluno cadastrado.");
+    return;
+  }
+
+  let texto = "--- LISTA DE ALUNOS ---\n";
+  for (let i = 0; i < alunos.length; i++) {
+    texto += `- ${alunos[i].nome}\n`;
+  }
+
+  alert(texto);
+}
+
